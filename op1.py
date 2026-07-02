8fe25eacf27b809886ef2b5538844a62d529efb7
Ben Chen <idben.chen@gmail.com>
2026-07-02 10:39:58 +0800

    餘數加上判斷式, 判斷奇偶數

--- op1.py
+++ op1.py
@@ -13,3 +13,19 @@ a = 10
 b = 3
 print(a/b)
 print(a//b)
+print("-"*20)
+
+# pass 放在判斷式後面當做暫時內容
+# 一種條件的判斷
+# 但是加上預設值，會變成 if-else 的變形
+result_a = "奇數"
+if a % 2 == 0:
+    result_a = "偶數"
+print(f"變數 a 是 {result_a}")
+
+# 兩種結果的判斷
+if b % 2 == 0:
+   result_b = "偶數"
+else:
+   result_b = "奇數"
+print(f"變數 b 是 {result_b}")
